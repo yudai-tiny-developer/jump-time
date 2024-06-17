19fc482e198f79936fc2629d65c52c7a4bcdae70
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-17 17:09:52 +0900

    v1.1.0

--- content.js
+++ content.js
@@ -18,14 +18,24 @@ function main() {
         }
     }).observe(app, { childList: true, subtree: true });
 
+    let prev_textContent;
+    let prev_paused;
+
     function apply_extention(time_current, video) {
         time_current.setAttribute('contenteditable', 'plaintext-only');
 
-        time_current.addEventListener('click', e => {
+        time_current.addEventListener('focusin', e => {
+            time_current.classList.add('_jump_time_edit');
+
+            prev_textContent = time_current.textContent;
+
+            prev_paused = video.paused;
             video.pause();
         });
 
-        time_current.addEventListener('blur', e => {
+        time_current.addEventListener('focusout', e => {
+            time_current.classList.remove('_jump_time_edit');
+
             const parts = time_current.textContent.split(':').map(v => parseInt(v));
             if (parts.length === 1 && !isNaN(parts[0])) {
                 video.currentTime = parts[0];
@@ -33,9 +43,13 @@ function main() {
                 video.currentTime = parts[0] * 60 + parts[1];
             } else if (parts.length === 3 && !isNaN(parts[0]) && !isNaN(parts[1]) && !isNaN(parts[2])) {
                 video.currentTime = parts[0] * 3600 + parts[1] * 60 + parts[2];
+            } else {
+                time_current.textContent = prev_textContent;
             }
 
-            video.play();
+            if (!prev_paused) {
+                video.play();
+            }
         });
 
         time_current.addEventListener('keydown', e => {
