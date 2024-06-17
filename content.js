6a0c899af82eeda8dec32c985a209ea59807c8fc
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2024-06-17 17:40:01 +0900

    V1.1.0 (#1)
    * isNaN
    
    * v1.1.0
    
    * Math.trunc
    
    * Escape
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- content.js
+++ content.js
@@ -18,34 +18,60 @@ function main() {
         }
     }).observe(app, { childList: true, subtree: true });
 
+    let time_current_confirm;
+    let prev_textContent;
+    let prev_paused;
+
     function apply_extention(time_current, video) {
         time_current.setAttribute('contenteditable', 'plaintext-only');
 
-        time_current.addEventListener('click', e => {
+        time_current.addEventListener('focusin', e => {
+            time_current.classList.add('_jump_time_edit');
+
+            prev_textContent = time_current.textContent;
+
+            prev_paused = video.paused;
             video.pause();
         });
 
-        time_current.addEventListener('blur', e => {
-            const parts = time_current.textContent.split(':').map(v => parseInt(v));
-            if (parts.length === 1 && !isNaN(parts[0])) {
-                video.currentTime = parts[0];
-            } else if (parts.length === 2) {
-                video.currentTime = parts[0] * 60 + parts[1];
-            } else if (parts.length === 3) {
-                video.currentTime = parts[0] * 3600 + parts[1] * 60 + parts[2];
+        time_current.addEventListener('focusout', e => {
+            time_current.classList.remove('_jump_time_edit');
+
+            if (time_current_confirm) {
+                const parts = time_current.textContent.split(':').map(v => Math.trunc(v));
+                if (parts.length === 1 && !isNaN(parts[0])) {
+                    video.currentTime = parts[0];
+                } else if (parts.length === 2 && !isNaN(parts[0]) && !isNaN(parts[1])) {
+                    video.currentTime = parts[0] * 60 + parts[1];
+                } else if (parts.length === 3 && !isNaN(parts[0]) && !isNaN(parts[1]) && !isNaN(parts[2])) {
+                    video.currentTime = parts[0] * 3600 + parts[1] * 60 + parts[2];
+                } else {
+                    time_current.innerText = prev_textContent;
+                }
+            } else {
+                time_current.innerText = prev_textContent;
             }
 
-            video.play();
+            if (!prev_paused) {
+                video.play();
+            }
         });
 
         time_current.addEventListener('keydown', e => {
-            if (document.activeElement === time_current) {
-                e.stopImmediatePropagation();
-            }
+            e.stopImmediatePropagation();
 
-            if (e.key === 'Enter') {
-                e.preventDefault();
-                time_current.blur();
+            switch (e.key) {
+                case 'Enter':
+                    e.preventDefault();
+                    time_current_confirm = true;
+                    time_current.blur();
+                    break;
+                case 'Escape':
+                    time_current_confirm = false;
+                    time_current.blur();
+                    break;
+                default:
+                    time_current_confirm = true;
             }
         });
     }
