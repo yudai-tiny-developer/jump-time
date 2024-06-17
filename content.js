4818884493c29298ea49483bd539f2f8646b1203
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-06-17 15:01:52 +0900

    v1.0.0

---
+++ content.js
@@ -0,0 +1,52 @@
+const app = document.querySelector('ytd-app');
+if (app) {
+    main();
+}
+
+function main() {
+    new MutationObserver((mutations, observer) => {
+        const player = app.querySelector('div#movie_player');
+        if (!player) {
+            return;
+        }
+
+        const time_current = player.querySelector('span.ytp-time-current');
+        const video = player.querySelector('video.video-stream');
+        if (time_current && video) {
+            observer.disconnect();
+            apply_extention(time_current, video);
+        }
+    }).observe(app, { childList: true, subtree: true });
+
+    function apply_extention(time_current, video) {
+        time_current.setAttribute('contenteditable', 'plaintext-only');
+
+        time_current.addEventListener('click', e => {
+            video.pause();
+        });
+
+        time_current.addEventListener('blur', e => {
+            const parts = time_current.textContent.split(':').map(v => parseInt(v));
+            if (parts.length === 1) {
+                video.currentTime = parts[0];
+            } else if (parts.length === 2) {
+                video.currentTime = parts[0] * 60 + parts[1];
+            } else if (parts.length === 3) {
+                video.currentTime = parts[0] * 3600 + parts[1] * 60 + parts[2];
+            }
+
+            video.play();
+        });
+
+        time_current.addEventListener('keydown', e => {
+            if (document.activeElement === time_current) {
+                e.stopImmediatePropagation();
+            }
+
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                time_current.blur();
+            }
+        });
+    }
+}
