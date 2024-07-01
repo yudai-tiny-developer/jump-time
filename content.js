b8d663a0daa224ba4959b53905565d99fcfcb7a3
yudai-tiny-developer <yudai-tiny-developer@gmail.com>
2024-07-02 03:00:59 +0900

    v1.1.2

--- content.js
+++ content.js
@@ -1,73 +1,79 @@
-const app = document.querySelector('ytd-app') ?? document.body;
-
 let time_current_confirm;
 let prev_textContent;
 let prev_paused;
 
-function apply_extention(time_current, video) {
-    time_current.setAttribute('contenteditable', 'plaintext-only');
+const app = document.querySelector('ytd-app') ?? document.body;
 
-    time_current.addEventListener('focusin', e => {
-        time_current.classList.add('_jump_time_edit');
+if (!window.location.href.startsWith('https://www.youtube.com/live_chat?')) {
+    main();
+}
 
-        prev_textContent = time_current.textContent;
+function main() {
+    function apply_extention(time_current, video) {
+        time_current.setAttribute('contenteditable', 'plaintext-only');
 
-        prev_paused = video.paused;
-        video.pause();
-    });
+        time_current.addEventListener('focusin', e => {
+            time_current.classList.add('_jump_time_edit');
 
-    time_current.addEventListener('focusout', e => {
-        time_current.classList.remove('_jump_time_edit');
+            prev_textContent = time_current.textContent;
 
-        if (time_current_confirm) {
-            const parts = time_current.textContent.split(':').map(v => Math.trunc(v));
-            if (parts.length === 1 && !isNaN(parts[0])) {
-                video.currentTime = parts[0];
-            } else if (parts.length === 2 && !isNaN(parts[0]) && !isNaN(parts[1])) {
-                video.currentTime = parts[0] * 60 + parts[1];
-            } else if (parts.length === 3 && !isNaN(parts[0]) && !isNaN(parts[1]) && !isNaN(parts[2])) {
-                video.currentTime = parts[0] * 3600 + parts[1] * 60 + parts[2];
+            prev_paused = video.paused;
+            video.pause();
+        });
+
+        time_current.addEventListener('focusout', e => {
+            time_current.classList.remove('_jump_time_edit');
+
+            if (time_current_confirm) {
+                const parts = time_current.textContent.split(':').map(v => Math.trunc(v));
+                if (parts.length === 1 && !isNaN(parts[0])) {
+                    video.currentTime = parts[0];
+                } else if (parts.length === 2 && !isNaN(parts[0]) && !isNaN(parts[1])) {
+                    video.currentTime = parts[0] * 60 + parts[1];
+                } else if (parts.length === 3 && !isNaN(parts[0]) && !isNaN(parts[1]) && !isNaN(parts[2])) {
+                    video.currentTime = parts[0] * 3600 + parts[1] * 60 + parts[2];
+                } else {
+                    time_current.innerText = prev_textContent;
+                }
             } else {
                 time_current.innerText = prev_textContent;
             }
-        } else {
-            time_current.innerText = prev_textContent;
+
+            if (!prev_paused) {
+                video.play();
+            }
+        });
+
+        time_current.addEventListener('keydown', e => {
+            e.stopImmediatePropagation();
+
+            switch (e.key) {
+                case 'Enter':
+                    e.preventDefault();
+                    time_current_confirm = true;
+                    time_current.blur();
+                    break;
+                case 'Escape':
+                    time_current_confirm = false;
+                    time_current.blur();
+                    break;
+                default:
+                    time_current_confirm = true;
+            }
+        });
+    }
+
+    new MutationObserver((mutations, observer) => {
+        const player = app.querySelector('div#movie_player');
+        if (!player) {
+            return;
         }
 
-        if (!prev_paused) {
-            video.play();
+        const time_current = player.querySelector('span.ytp-time-current');
+        const video = player.querySelector('video.video-stream');
+        if (time_current && video) {
+            observer.disconnect();
+            apply_extention(time_current, video);
         }
-    });
-
-    time_current.addEventListener('keydown', e => {
-        e.stopImmediatePropagation();
-
-        switch (e.key) {
-            case 'Enter':
-                e.preventDefault();
-                time_current_confirm = true;
-                time_current.blur();
-                break;
-            case 'Escape':
-                time_current_confirm = false;
-                time_current.blur();
-                break;
-            default:
-                time_current_confirm = true;
-        }
-    });
+    }).observe(app, { childList: true, subtree: true });
 }
-
-new MutationObserver((mutations, observer) => {
-    const player = app.querySelector('div#movie_player');
-    if (!player) {
-        return;
-    }
-
-    const time_current = player.querySelector('span.ytp-time-current');
-    const video = player.querySelector('video.video-stream');
-    if (time_current && video) {
-        observer.disconnect();
-        apply_extention(time_current, video);
-    }
-}).observe(app, { childList: true, subtree: true });
